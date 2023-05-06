17077ae1d09f2ae4c1fbcacc4eeff6481870ed5a
Caleb Dalgleish <calebdalgleish@Calebs-MacBook-Air.local>
2023-05-06 14:25:41 -0500

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,9 @@ const displayGitHubData = async () => {
     document.querySelector('#github-repo-count').textContent = data.public_repos;
 };
 
-// Fetch and display LinkedIn data (Note: LinkedIn API requires OAuth2 for authentication)
-// This is just a basic example, you will need to properly set up OAuth2 authentication and permissions.
-const displayLinkedInData = async () => {
-    // const accessToken = 'access_token';
-    // const linkedInUrl = 'https://api.linkedin.com/v2/me?oauth2_access_token=' + accessToken;
-    // const data = await fetchData(linkedInUrl);
-
-    // Update your website content using the fetched data
-    // document.querySelector('#linkedin-username').textContent = data.localizedFirstName + ' ' + data.localizedLastName;
-    // document.querySelector('#linkedin-profile-link').href = data.vanityName;
-};
-
 // Initialize the website
 const init = async () => {
     await displayGitHubData();
-    await displayLinkedInData();
 };
 
 init();
