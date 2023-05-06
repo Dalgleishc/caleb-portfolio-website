46bedb3917f6fae4fcdd275d6e58c7f0c441a4e5
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2023-05-06 13:21:25 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
+const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+const displayGitHubData = async () => {
+    const githubUrl = 'https://api.github.com/users/dalgleishc';
+    const data = await fetchData(githubUrl);
+
+    // Update your website content using the fetched data
+};
+
+const displayLinkedInData = async () => {
+    // Make sure you follow LinkedIn's API documentation and register your app to get access
+    const linkedInUrl = 'https://api.linkedin.com/v2/me';
+    const data = await fetchData(linkedInUrl);
+
+    // Update your website content using the fetched data
+};
+
+displayGitHubData();
+displayLinkedInData();
 
