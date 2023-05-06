93d0d6cf6879115bc985f2f3afba140604cb43e8
Caleb Dalgleish <calebdalgleish@Calebs-MacBook-Air.local>
2023-05-06 13:57:15 -0500

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,33 @@
-const fetchData = async (url) => {
-    try {
-        const response = await fetch(url);
-        return response.json();
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-};
+import { fetchData } from './utils.js';
 
+// Fetch and display GitHub data
 const displayGitHubData = async () => {
     const githubUrl = 'https://api.github.com/users/dalgleishc';
     const data = await fetchData(githubUrl);
 
     // Update your website content using the fetched data
+    document.querySelector('#github-username').textContent = data.login;
+    document.querySelector('#github-profile-link').href = data.html_url;
+    document.querySelector('#github-avatar').src = data.avatar_url;
+    document.querySelector('#github-repo-count').textContent = data.public_repos;
 };
 
+// Fetch and display LinkedIn data (Note: LinkedIn API requires OAuth2 for authentication)
+// This is just a basic example, you will need to properly set up OAuth2 authentication and permissions.
 const displayLinkedInData = async () => {
-    // Make sure you follow LinkedIn's API documentation and register your app to get access
-    const linkedInUrl = 'https://api.linkedin.com/v2/me';
-    const data = await fetchData(linkedInUrl);
+    // const accessToken = 'access_token';
+    // const linkedInUrl = 'https://api.linkedin.com/v2/me?oauth2_access_token=' + accessToken;
+    // const data = await fetchData(linkedInUrl);
 
     // Update your website content using the fetched data
+    // document.querySelector('#linkedin-username').textContent = data.localizedFirstName + ' ' + data.localizedLastName;
+    // document.querySelector('#linkedin-profile-link').href = data.vanityName;
 };
 
-displayGitHubData();
-displayLinkedInData();
+// Initialize the website
+const init = async () => {
+    await displayGitHubData();
+    await displayLinkedInData();
+};
 
+init();
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,13 @@
+export const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
 
+
+// The utils.js file exports the fetchData function for reuse in main.js. 
+// Remember to replace 'your_username' with your actual GitHub username, and set up
+// proper OAuth2 authentication for the LinkedIn API.s
